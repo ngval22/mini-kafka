deb9f3589f5bcc7b4bafeae4d4b20ba5b8c9a3f5
ngval22 <ngval22@freeuni.edu.ge>
2025-12-08 14:49:15 +0400

    wip: threads

--- include/mini_kafka/broker.h
+++ include/mini_kafka/broker.h
@@ -1,8 +1,13 @@
 #pragma once
 
+#include <condition_variable>
 #include <cstddef>
 #include <cstdint>
+#include <mutex>
+#include <queue>
 #include <string>
+#include <thread>
+#include <vector>
 
 #include "mini_kafka/partition_store.h"
 #include "mini_kafka/topic.h"
@@ -26,11 +31,30 @@ public:
     void serve_n(std::size_t max_connections);
 
 private:
-    void serve_one();
+    static constexpr std::size_t k_worker_count = 4;
+
+    void handle_client(int client_fd);
+    void worker_loop();
+    void start_workers();
+    void stop_workers();
+    void enqueue_client(int client_fd);
+    void on_client_handled();
 
     PartitionLogStore store_;
     int listen_fd_;
     uint16_t port_;
+
+    std::vector<std::thread> workers_;
+    std::queue<int> client_queue_;
+    std::mutex queue_mutex_;
+    std::condition_variable queue_cv_;
+    bool workers_running_ = false;
+    bool stopping_ = false;
+
+    std::mutex completion_mutex_;
+    std::condition_variable completion_cv_;
+    bool track_job_completion_ = false;
+    std::size_t jobs_remaining_ = 0;
 };
 
 }  // namespace mini_kafka
--- src/broker.cpp
+++ src/broker.cpp
@@ -47,6 +47,7 @@ Broker::Broker(std::string data_dir, uint16_t port)
 }
 
 Broker::~Broker() {
+    stop_workers();
     close_fd(listen_fd_);
 }
 
@@ -58,21 +59,76 @@ void Broker::add_topic(TopicMetadata topic) {
     store_.add_topic(std::move(topic));
 }
 
-void Broker::serve_forever() {
+void Broker::start_workers() {
+    if (workers_running_) {
+        return;
+    }
+    stopping_ = false;
+    workers_running_ = true;
+    workers_.reserve(k_worker_count);
+    for (std::size_t i = 0; i < k_worker_count; ++i) {
+        workers_.emplace_back(&Broker::worker_loop, this);
+    }
+}
+
+void Broker::stop_workers() {
+    if (!workers_running_) {
+        return;
+    }
+    {
+        std::lock_guard<std::mutex> lock(queue_mutex_);
+        stopping_ = true;
+    }
+    queue_cv_.notify_all();
+    for (std::thread& worker : workers_) {
+        worker.join();
+    }
+    workers_.clear();
+    workers_running_ = false;
+    stopping_ = false;
+}
+
+void Broker::enqueue_client(int client_fd) {
+    {
+        std::lock_guard<std::mutex> lock(queue_mutex_);
+        client_queue_.push(client_fd);
+    }
+    queue_cv_.notify_one();
+}
+
+void Broker::on_client_handled() {
+    if (!track_job_completion_) {
+        return;
+    }
+    std::lock_guard<std::mutex> lock(completion_mutex_);
+    if (jobs_remaining_ == 0) {
+        return;
+    }
+    --jobs_remaining_;
+    if (jobs_remaining_ == 0) {
+        completion_cv_.notify_one();
+    }
+}
+
+void Broker::worker_loop() {
     while (true) {
-        serve_one();
+        int client_fd = -1;
+        {
+            std::unique_lock<std::mutex> lock(queue_mutex_);
+            queue_cv_.wait(lock, [&] { return stopping_ || !client_queue_.empty(); });
+            if (stopping_ && client_queue_.empty()) {
+                return;
+            }
+            client_fd = client_queue_.front();
+            client_queue_.pop();
+        }
+        handle_client(client_fd);
+        on_client_handled();
     }
 }
 
-void Broker::serve_n(std::size_t max_connections) {
-    for (std::size_t i = 0; i < max_connections; ++i) {
-        serve_one();
-    }
-}
-
-void Broker::serve_one() {
-    SocketHandle client(accept_client(listen_fd_));
-
+void Broker::handle_client(int client_fd) {
+    SocketHandle client(client_fd);
     try {
         const std::vector<uint8_t> request = read_frame(client.get());
         const std::vector<uint8_t> response = handle_request(store_, request);
@@ -80,6 +136,35 @@ void Broker::serve_one() {
     } catch (const std::exception& ex) {
         write_frame(client.get(), encode_error_response(ex.what()));
     }
+}
+
+void Broker::serve_forever() {
+    start_workers();
+    while (true) {
+        const int client_fd = accept_client(listen_fd_);
+        enqueue_client(client_fd);
+    }
+}
+
+void Broker::serve_n(std::size_t max_connections) {
+    start_workers();
+    {
+        std::lock_guard<std::mutex> lock(completion_mutex_);
+        track_job_completion_ = true;
+        jobs_remaining_ = max_connections;
+    }
+
+    for (std::size_t i = 0; i < max_connections; ++i) {
+        const int client_fd = accept_client(listen_fd_);
+        enqueue_client(client_fd);
+    }
+
+    {
+        std::unique_lock<std::mutex> lock(completion_mutex_);
+        completion_cv_.wait(lock, [&] { return jobs_remaining_ == 0; });
+        track_job_completion_ = false;
+    }
+    stop_workers();
 }
 
 }  // namespace mini_kafka
